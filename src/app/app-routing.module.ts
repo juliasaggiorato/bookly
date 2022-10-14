1c923817b50106068c373a60bc1b7e15f885e16d
Gonzalo Bechara <60068962+cacalo@users.noreply.github.com>
2022-10-14 16:34:40 -0300

    Cambios hechos en clase por el profe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,23 +4,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () =>
-      import('./tabs/tabs.module').then((m) => m.TabsPageModule),
+    loadChildren: () => import('./public/pages/login/login.module').then( m => m.LoginPageModule)
   },
   {
     path: 'homepage',
     loadChildren: () => import('./public/pages/homepage/homepage.module').then( m => m.HomepagePageModule)
   },
   {
-    path: 'login',
-    loadChildren: () => import('./public/pages/login/login.module').then( m => m.LoginPageModule)
-  },
-  {
     path: 'perfil',
     loadChildren: () => import('./public/pages/perfil/perfil.module').then( m => m.PerfilPageModule)
   },
   {
-    path: 'libro1',
+    path: 'libro/:id',
     loadChildren: () => import('./public/pages/libro1/libro1.module').then( m => m.Libro1PageModule)
   },
   {
--- src/app/public/pages/libro1/libro1.page.ts
+++ src/app/public/pages/libro1/libro1.page.ts
@@ -12,4 +12,9 @@ export class Libro1Page implements OnInit {
   ngOnInit() {
   }
 
+  libro = {
+    titulo: 'Una Corte de Rosas y Espinas',
+    discripcion: 'Feyre está desesperada, su vida y la de su familia dependen de ella.    Enfrentada al hambre más absoluto, no dudará en ir al bosque prohibido y    matar si es necesario. Pero su osadía la convierte en prisionera del    misterioso Tamlin, quien a pesar de su aparente frialdad la hará descubrir    una ardiente pasión que marcará su destino. Lejos de su familia y su mundo,    Feyre tendrá que tomar una decisión capital para salvar todo lo que ama.' 
+  }
+
 }
