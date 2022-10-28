84d387866efdf5e97190b6e3cc3e32ecab05688e
juliviceconti <julieta.viceconti@hotmail.com>
2022-10-28 12:39:16 -0300

    cambio de posiciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,27 +4,45 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./public/pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () =>
+      import('./public/pages/login/login.module').then(
+        (m) => m.LoginPageModule
+      ),
   },
   {
     path: 'homepage',
-    loadChildren: () => import('./public/pages/homepage/homepage.module').then( m => m.HomepagePageModule)
+    loadChildren: () =>
+      import('./public/pages/homepage/homepage.module').then(
+        (m) => m.HomepagePageModule
+      ),
   },
   {
     path: 'perfil',
-    loadChildren: () => import('./public/pages/perfil/perfil.module').then( m => m.PerfilPageModule)
+    loadChildren: () =>
+      import('./public/pages/perfil/perfil.module').then(
+        (m) => m.PerfilPageModule
+      ),
   },
   {
     path: 'libro/:id',
-    loadChildren: () => import('./public/pages/libro1/libro1.module').then( m => m.Libro1PageModule)
+    loadChildren: () =>
+      import('./public/pages/libro1/libro1.module').then(
+        (m) => m.Libro1PageModule
+      ),
   },
   {
     path: 'signup',
-    loadChildren: () => import('./public/pages/signup/signup.module').then( m => m.SignupPageModule)
+    loadChildren: () =>
+      import('./public/pages/signup/signup.module').then(
+        (m) => m.SignupPageModule
+      ),
   },
   {
     path: 'settings',
-    loadChildren: () => import('./public/pages/settings/settings.module').then( m => m.SettingsPageModule)
+    loadChildren: () =>
+      import('./public/pages/settings/settings.module').then(
+        (m) => m.SettingsPageModule
+      ),
   },
 ];
 @NgModule({
