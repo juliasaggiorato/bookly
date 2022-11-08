6d5fb54a6c6083589b0957a7b7a1f8a261823d7f
juliviceconti <julieta.viceconti@hotmail.com>
2022-11-08 19:02:47 -0300

    terminando el merge

--- src/app/public/pages/homepage/homepage.page.ts
+++ src/app/public/pages/homepage/homepage.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { LibrosService } from 'src/app/core/services/libros.service';
 
 @Component({
   selector: 'app-homepage',
@@ -8,15 +7,9 @@ import { LibrosService } from 'src/app/core/services/libros.service';
 })
 export class HomepagePage implements OnInit {
 
-  constructor(private librosService:LibrosService) { }
-libros=[]
-ngOnInit() {
-  this.getLibros()
+  constructor() { }
 
-  }
-  async getLibros(){
-   this.libros= await this.librosService.getLibros()
-    console.log(this.libros)
+  ngOnInit() {
   }
 
 }
