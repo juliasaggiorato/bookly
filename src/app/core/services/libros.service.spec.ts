a5924d521c77a4993bf21922c907220c6b09f606
juliasaggiorato <106684085+juliasaggiorato@users.noreply.github.com>
2022-11-08 18:32:44 -0300

    cambios consulta gonza

--- src/app/core/services/libros.service.spec.ts
+++ src/app/core/services/libros.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ServiciosService } from './servicios.service';
+import { LibrosService } from './libros.service';
 
-describe('ServiciosService', () => {
-  let service: ServiciosService;
+describe('LibrosService', () => {
+  let service: LibrosService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ServiciosService);
+    service = TestBed.inject(LibrosService);
   });
 
   it('should be created', () => {
--- src/app/public/pages/homepage/homepage.page.ts
+++ src/app/public/pages/homepage/homepage.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LibrosService } from 'src/app/core/services/libros.service';
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepagePage implements OnInit {
 
-  constructor() { }
+  constructor(private librosService:LibrosService) { }
+libros=[]
+ngOnInit() {
+  this.getLibros()
 
-  ngOnInit() {
+  }
+  async getLibros(){
+   this.libros= await this.librosService.getLibros()
+    console.log(this.libros)
   }
 
 }
