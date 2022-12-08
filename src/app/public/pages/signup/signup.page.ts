5d9b609c6368d6218dbd1b2b937403b1cd37e977
juliviceconti <julieta.viceconti@hotmail.com>
2022-12-08 17:49:03 -0300

    intentando signup

--- src/app/public/pages/signup/signup.page.ts
+++ src/app/public/pages/signup/signup.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  usuario: string = 'Usuario';
+  email: string = 'Email';
+  contrasena: string = 'Contraseña';
 
   ngOnInit() {
+    this.usuario = this.usuarioInput.value;
+    this.email = this.emailInput.value;
+    this.contrasena = this.contrasenaInput.value;
+
+    this.http
+      .post('http://localhost:8080/usuario', {
+        nombre: this.usuario,
+        email: this.email,
+        password: this.contrasena,
+      })
+      .subscribe((response) => {});
   }
 
+  @ViewChild('#usuarioInput') usuarioInput: HTMLInputElement;
+  @ViewChild('#emailInput') emailInput: HTMLInputElement;
+  @ViewChild('#contrasenaInput') contrasenaInput: HTMLInputElement;
 }
